5324e416bb555a8290843ff25a09026e09facae8
berkantavs <berkantavsar7@gmail.com>
2022-01-14 17:07:24 +0300

    choose shopping center and dashboard index created

---
+++ pages/ChooseShoppingCenter.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>choose shopping center</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,139 +1,162 @@
 <template>
   <body class="bg-login">
-    <div class="wrapper">
-      <div
-        class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0"
-      >
-        <div class="container-fluid">
-          <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-            <div class="col mx-auto">
-              <div class="mb-4 text-center">
-                <img src="@/assets/images/logo-img.png" width="180" alt="" />
-              </div>
-              <div class="card">
-                <div class="card-body">
-                  <div class="border p-4 rounded">
-                    <div class="text-center">
-                      <h3 class="">Sign in</h3>
-                      <p>
-                        Don't have an account yet?
-                        <a href="authentication-signup.html">Sign up here</a>
-                      </p>
-                    </div>
-                    <div class="d-grid">
-                      <a
-                        class="btn my-4 shadow-sm btn-white"
-                        href="javascript:;"
-                      >
-                        <span
-                          class="d-flex justify-content-center align-items-center"
-                        >
-                          <img
-                            class="me-2"
-                            src="@/assets/images/icons/search.svg"
-                            width="16"
-                            alt="Image Description"
-                          />
-                          <span>Sign in with Google</span>
-                        </span>
-                      </a>
-                      <a href="javascript:;" class="btn btn-facebook"
-                        ><i class="bx bxl-facebook"></i>Sign in with Facebook</a
-                      >
-                    </div>
-                    <div class="login-separater text-center mb-4">
-                      <span>OR SIGN IN WITH EMAIL</span>
-                      <hr />
-                    </div>
-                    <div class="form-body">
-                      <form class="row g-3">
-                        <div class="col-12">
-                          <label for="inputEmailAddress" class="form-label"
-                            >Email Address</label
-                          >
-                          <input
-                            type="email"
-                            class="form-control"
-                            v-model="email"
-                            placeholder="Email Address"
-                          />
-                        </div>
-                        <div class="col-12">
-                          <label for="inputChoosePassword" class="form-label"
-                            >Enter Password</label
-                          >
-                          <div class="input-group" id="show_hide_password">
-                            <input
-                              type="password"
-                              class="form-control border-end-0"
-                              v-model="password"
-                              placeholder="Enter Password"
-                            />
-                            <a
-                              href="javascript:;"
-                              class="input-group-text bg-transparent"
-                              ><i class="bx bx-hide"></i
-                            ></a>
-                          </div>
-                        </div>
-                        <div class="col-md-6">
-                          <div class="form-check form-switch">
-                            <input
-                              class="form-check-input"
-                              type="checkbox"
-                              id="flexSwitchCheckChecked"
-                              checked
-                            />
-                            <label
-                              class="form-check-label"
-                              for="flexSwitchCheckChecked"
-                              >Remember Me</label
+    <template v-if="selected_shopping_center">
+      <div class="wrapper">
+        <div
+          class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0"
+        >
+          <div class="container-fluid">
+            <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+              <div class="col mx-auto">
+                <div class="mb-4 text-center">
+                  <img src="@/assets/images/logo.png" width="180" alt="" />
+                </div>
+                <div class="card">
+                  <div class="card-body">
+                    <div class="border p-4 rounded">
+                      <div class="form-body">
+                        <form class="row g-3">
+                          <div class="col-12">
+                            <label for="inputEmailAddress" class="form-label"
+                              >Email</label
                             >
+                            <input
+                              type="email"
+                              class="form-control"
+                              v-model="email"
+                              placeholder="Email Address"
+                            />
                           </div>
-                        </div>
-                        <div class="col-md-6 text-end">
-                          <a href="authentication-forgot-password.html"
-                            >Forgot Password ?</a
-                          >
-                        </div>
-                        <div class="col-12">
-                          <div class="d-grid">
-                            <button class="btn btn-primary" @click="login()">
-                              <i class="bx bxs-lock-open"></i>Sign in
-                            </button>
+                          <div class="col-12">
+                            <label for="inputChoosePassword" class="form-label"
+                              >Password</label
+                            >
+                            <div class="input-group" id="show_hide_password">
+                              <input
+                                type="password"
+                                class="form-control border-end-0"
+                                v-model="password"
+                                placeholder="Enter Password"
+                              />
+                              <a
+                                href="javascript:;"
+                                class="input-group-text bg-transparent"
+                                ><i class="bx bx-hide"></i
+                              ></a>
+                            </div>
                           </div>
-                        </div>
-                      </form>
+
+                          <div class="col-12">
+                            <div class="d-grid">
+                              <b-button variant="primary" @click="login()">
+                                <i class="bx bxs-lock-open"></i>Sign in
+                              </b-button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
+            <!--end row-->
           </div>
-          <!--end row-->
         </div>
       </div>
-    </div>
+    </template>
+    <template v-else>
+      <div class="choose-shopping-center text-center m-5">
+        <img src="@/assets/images/logo.png" width="180" alt="" />
+        <h5 class="d-block mt-2 mb-3">Choose Shopping Center</h5>
+        <div class="shopping-centers mt-2">
+          <b-row>
+            <b-col md="6" offset-md="3">
+              <b-row>
+                <b-col lg="6">
+                  <b-card
+                    no-body
+                    @click="selectShoppingCenter('koctas')"
+                    class="rounded border cursor-pointer text-center shopping-center-card"
+                  >
+                    <div class="text-center">
+                      <img
+                        src="@/assets/images/koctas.png"
+                        width="180"
+                        alt=""
+                      />
+                    </div>
+                  </b-card>
+                </b-col>
+                <b-col lg="6">
+                  <b-card
+                    no-body
+                    @click="selectShoppingCenter('civil')"
+                    class="rounded border cursor-pointer text-center shopping-center-card"
+                  >
+                    <div class="text-center">
+                      <img src="@/assets/images/civil.png" width="180" alt="" />
+                    </div> </b-card
+                ></b-col>
+                <b-col lg="6">
+                  <b-card
+                    no-body
+                    @click="selectShoppingCenter('ebebek')"
+                    class="rounded border cursor-pointer text-center shopping-center-card"
+                  >
+                    <div class="text-center">
+                      <img
+                        src="@/assets/images/ebebek.png"
+                        width="180"
+                        alt=""
+                      />
+                    </div>
+                  </b-card>
+                </b-col>
+                <b-col lg="6">
+                  <b-card
+                    no-body
+                    @click="selectShoppingCenter('evidea')"
+                    class="rounded border text-center cursor-pointer shopping-center-card"
+                  >
+                    <div class="text-center">
+                      <img
+                        src="@/assets/images/evidea.png"
+                        width="180"
+                        alt=""
+                      /></div></b-card
+                ></b-col>
+              </b-row>
+            </b-col>
+          </b-row>
+        </div>
+      </div>
+    </template>
   </body>
 </template>
 
 <script>
 export default {
   data() {
-    return { email: null, password: null }
+    return { email: null, password: null, selected_shopping_center: null }
   },
   methods: {
-    authCheck() {
-      if (this.email == 'info@ebebek.com' && this.password == 'ebebek123') {
+    authCheck(email, password) {
+      if (email == 'info@ebebek.com' && password == 'ebebek123') {
         return true
       } else {
         return false
       }
     },
     login() {
-      if (this.authCheck()) {
-        console.log('sa');
-        this.$router.push('/dashboard');
+      if (this.authCheck(this.email, this.password)) {
+        this.$router.push('/dashboard')
+      }
+    },
+    selectShoppingCenter(s_center) {
+      if (s_center) {
+        localStorage.setItem('shopping_center', s_center)
+        this.selected_shopping_center = s_center
       }
     },
   },
