51cb2efb71c9026d1f8ca4eed4767d887068acda
berkantavs <berkantavsar7@gmail.com>
2022-01-14 11:46:29 +0300

    login has been created

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   head: {
     title: 'dashboard-example',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'tr',
     },
     meta: [
       { charset: 'utf-8' },
@@ -15,7 +15,16 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/app.css',
+  '@/assets/css/bootstrap-extended.css',
+  '@/assets/css/bootstrap.css',
+  '@/assets/css/bootstrap.min.css',
+  '@/assets/css/dark-theme.css',
+  '@/assets/css/header-colors.css',
+  '@/assets/css/icons.css',
+  '@/assets/css/pace.min.css',
+  '@/assets/css/semi-dark.css',],
+
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
